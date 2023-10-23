b0ff39c14ca416c68472e50f5f17df58bc22bb25
vegadelalyra <lyradeperseo@gmail.com>
2023-10-23 17:32:39 -0500

    feat: implement 10 tests for models and views

--- poll/urls.py
+++ poll/urls.py
@@ -5,12 +5,11 @@ from . import views
 app_name = 'poll'
 urlpatterns = [
     # ex: /fun_polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /fun_polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /fun_polls/5/results
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /fun_polls/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /fun_polls/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /fun_polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
-
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,24 +1,43 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from typing import Any
+from django.db import models
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
+
+from django.db.models import F
 
 from .models import Choice, Question
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list 
-    }
-    return render(request, 'poll/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'poll/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'poll/detail.html', {'question': question})
+    def get_queryset(self):
+        """
+        Return the last five published questions.
+        (not including those set to be published in the future).
+        """ 
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'poll/detail.html'
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'poll/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -34,7 +53,7 @@ def vote(request, question_id):
             },
         )
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('poll:results', args=(question.id,)))
 
