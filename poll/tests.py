2adfe12be781e79e5ccb65e43a85159233cbb969
vegadelalyra <lyradeperseo@gmail.com>
2023-10-23 17:40:16 -0500

    test: implement results view test succesfully

--- poll/tests.py
+++ poll/tests.py
@@ -104,7 +104,7 @@ class QuestionIndexViewTests(TestCase):
         self.assertQuerySetEqual(response.context['latest_question_list'], [question2, question1])
 
     
-class QuestionDetailViewTests(TestCase):J
+class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
         """
         The detail view of a question with a pub_date in the future returns a 404 not found.
@@ -120,5 +120,25 @@ class QuestionDetailViewTests(TestCase):J
         """    
         past_question = create_question(question_text='Past Question.', days=-5)
         url = reverse('poll:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+
+class QuestionResultsViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The results view of a question with a pub_date in the future returns a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('poll:results', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The results view of a question with a pub_date in the past displays the question's text.
+        """    
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('poll:results', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
